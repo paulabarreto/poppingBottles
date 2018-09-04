fe7ca073dce614a3c598c41382aa52e0fb3465d6
paulabarreto <paulavilaca@gmail.com>
2018-09-04 02:28:58 +0000

    trying to find algorithm - working for 0 investment

--- poppingBottles.js
+++ poppingBottles.js
@@ -9,8 +9,20 @@
 function poppingBottles(investment){
   let recycle = investment / 2;
   var getBackBottle = Math.floor(recycle / 2);
+  var leftOver = Math.round(recycle % 2);
+  var partialTotal = getBackBottle + leftOver;
+  var getMoreBottle = Math.floor(partialTotal / 2);
+  var getEvenMore = (getMoreBottle + leftOver) / 2;
 
-  return getBackBottle;
+  var sum = recycle + getBackBottle + leftOver + getMoreBottle + getEvenMore;
+
+  var bottlesFromCaps = Math.floor(sum / 4);
+  var gettingMore = Math.floor(bottlesFromCaps / 2);
+  var moreBottlesfromCaps = Math.floor(bottlesFromCaps + gettingMore / 2);
+  var finalMath = Math.round(gettingMore % 2) + Math.round(moreBottlesfromCaps % 2)
+
+  var finalSum = bottlesFromCaps + gettingMore + moreBottlesfromCaps + sum;
+  return finalSum;
 }
 
-console.log(poppingBottles(10));
+console.log(poppingBottles(30));
